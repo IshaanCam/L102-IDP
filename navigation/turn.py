267110ea04951926d5dc0a88509ec84f071dc527
Ishaan Agarwal <IshaanAgarwal2@dhcp-10-248-141-57.eduroam.wireless.private.cam.ac.uk>
2026-01-29 10:48:10 +0000

    added turning

--- navigation/turn.py
+++ navigation/turn.py
@@ -1,12 +1,14 @@
+from utime import sleep
+
 def turn(direction: str, right_motor, left_motor, junction_detected) -> None:
     right_motor.Stop()
     left_motor.Stop()
     motor = right_motor
     if direction == "left":
         motor = left_motor
-    while not junction_detected:
-        if direction == "right":
-            motor.Forward()
-        else:
-            motor.Forward()
+    if direction == "right":
+        motor.Forward()
+    else:
+        motor.Forward()
+    sleep(1)
     motor.Stop()
