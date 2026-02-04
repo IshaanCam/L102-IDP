985130223b9c6863b592725cf40e6d48a05a9438
Ishaan Agarwal <IshaanAgarwal2@dhcp-10-248-141-247.eduroam.wireless.private.cam.ac.uk>
2026-02-04 16:28:49 +0000

    updated path

--- navigation/path.py
+++ navigation/path.py
@@ -1,37 +1,37 @@
 path = {
     ("start", "bay_3"): ["j1", "bay_3_entrance"],
 
-    ("bay_3", "upper_a"): ["bay_3_entrance", "bay_4_entrance", "entrance_lower_b", "back_right_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
+    ("bay_3", "upper_a"): ["bay_3_entrance", "bay_4_entrance", "entrance_lower_b", "entrance_lower_b2", "entrance_lower_b3", "entrance_lower_b4", "entrance_lower_b5", "entrance_lower_b6", "exit_lower_b", "back_right_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
     ("bay_3", "upper_b"): ["bay_3_entrance", "bay_4_entrance", "entrance_lower_b", "back_right_corner", "ramp", "top_of_ramp", "corner_before_upper_b"],
     ("bay_3", "lower_b"): ["bay_3_entrance", "bay_4_entrance"],
     ("bay_3", "lower_a"): ["bay_3_entrance", "j1", "bay_2_entrance", "bay_1_entrance"],
 
-    ("lower_a", "bay4"): ["bay_1_entrance", "bay_2_entrance", "j1", "bay_3_entrance", "bay_4_entrance"],
-    ("lower_b", "bay4"): ["bay_4_entrance"],
-    ("upper_a", "bay4"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_right_corner", "exit_lower_b", "bay_4_entrance"],
-    ("upper_b", "bay4"): ["corner_before_upper_b", "top_of_ramp", "ramp", "back_right_corner", "exit_lower_b", "bay_4_entrance"],
+    ("lower_a", "bay_4"): ["bay_1_entrance", "bay_2_entrance", "j1", "bay_3_entrance", "bay_4_entrance"],
+    ("lower_b", "bay_4"): ["bay_4_entrance"],
+    ("upper_a", "bay_4"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_right_corner", "exit_lower_b", "entrance_lower_b6", "entrance_lower_b5", "entrance_lower_b4", "entrance_lower_b3", "entrance_lower_b2", "entrance_lower_b", "bay_4_entrance"],
+    ("upper_b", "bay_4"): ["corner_before_upper_b", "top_of_ramp", "ramp", "back_right_corner", "exit_lower_b", "bay_4_entrance"],
 
-    ("bay_4", "upper_a"): ["bay_4_entrance", "entrance_lower_b", "back_right_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
+    ("bay_4", "upper_a"): ["bay_4_entrance", "entrance_lower_b", "entrance_lower_b2", "entrance_lower_b3", "entrance_lower_b4", "entrance_lower_b5", "entrance_lower_b6", "exit_lower_b", "back_right_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
     ("bay_4", "upper_b"): ["bay_4_entrance", "entrance_lower_b", "back_right_corner", "ramp", "top_of_ramp", "corner_before_upper_b"],
-    ("bay_4", "lower_a"): ["bay_4_entrance"],
-    ("bay_4", "lower_b"): ["bay_4_entrance", "bay_3_entrance", "j1", "bay_2_entrance", "bay_1_entrance"],
+    ("bay_4", "lower_b"): ["bay_4_entrance"],
+    ("bay_4", "lower_a"): ["bay_4_entrance", "bay_3_entrance", "j1", "bay_2_entrance", "bay_1_entrance"],
 
-    ("lower_a", "bay1"): ["bay_1_entrance"],
-    ("lower_b", "bay1"): ["bay_4_entrance", "bay_3_entrance", "j1", "bay_2_entrance", "bay_1_entrance"],
-    ("upper_a", "bay1"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a", "bay_1_entrance"],
-    ("upper_b", "bay1"): ["corner_before_upper_b", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a", "bay_1_entrance"],
+    ("lower_a", "bay_1"): ["bay_1_entrance"],
+    ("lower_b", "bay_1"): ["bay_4_entrance", "bay_3_entrance", "j1", "bay_2_entrance", "bay_1_entrance"],
+    ("upper_a", "bay_1"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a","entrance_lower_a6", "entrance_lower_a5", "entrance_lower_a4", "entrance_lower_a3", "entrance_lower_a2", "entrance_lower_a", "bay_1_entrance"],
+    ("upper_b", "bay_1"): ["corner_before_upper_b", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a", "bay_1_entrance"],
 
-    ("bay_1", "upper_a"): ["bay_1_entrance", "entrance_lower_a", "back_left_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
+    ("bay_1", "upper_a"): ["bay_1_entrance", "entrance_lower_a", "entrance_lower_a2", "entrance_lower_a3", "entrance_lower_a4", "entrance_lower_a5", "entrance_lower_a6", "exit_lower_a", "back_left_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
     ("bay_1", "upper_b"): ["bay_1_entrance", "entrance_lower_a", "back_left_corner", "ramp", "top_of_ramp", "corner_before_upper_b"],
     ("bay_1", "lower_a"): ["bay_1_entrance"],
     ("bay_1", "lower_b"): ["bay_1_entrance", "bay_2_entrance", "j1", "bay_3_entrance", "bay_4_entrance"],
 
-    ("lower_a", "bay2"): ["bay_1_entrance", "bay_2_entrance"],
-    ("lower_b", "bay2"): ["bay_4_entrance", "bay_3_entrance", "j1", "bay_2_entrance"],
-    ("upper_a", "bay2"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a", "bay_1_entrance", "bay_2_entrance"],
-    ("upper_b", "bay2"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a", "bay_1_entrance", "bay_2_entrance"],
+    ("lower_a", "bay_2"): ["bay_1_entrance", "bay_2_entrance"],
+    ("lower_b", "bay_2"): ["bay_4_entrance", "bay_3_entrance", "j1", "bay_2_entrance"],
+    ("upper_a", "bay_2"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a","entrance_lower_a6", "entrance_lower_a5", "entrance_lower_a4", "entrance_lower_a3", "entrance_lower_a2", "entrance_lower_a", "bay_1_entrance", "bay_2_entrance"],
+    ("upper_b", "bay_2"): ["corner_before_upper_a", "top_of_ramp", "ramp", "back_left_corner", "exit_lower_a", "bay_1_entrance", "bay_2_entrance"],
 
-    ("bay_2", "upper_a"): ["bay_2_entrance", "bay_1_entrance", "entrance_lower_a", "back_left_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
+    ("bay_2", "upper_a"): ["bay_2_entrance", "bay_1_entrance", "entrance_lower_a",  "entrance_lower_a2", "entrance_lower_a3", "entrance_lower_a4", "entrance_lower_a5", "entrance_lower_a6", "exit_lower_a", "back_left_corner", "ramp", "top_of_ramp", "corner_before_upper_a"],
     ("bay_2", "upper_b"): ["bay_2_entrance", "bay_1_entrance", "entrance_lower_a", "back_left_corner", "ramp", "top_of_ramp", "corner_before_upper_b"],
     ("bay_2", "lower_a"): ["bay_2_entrance", "bay_1_entrance"],
     ("bay_2", "lower_b"): ["bay_2_entrance", "j1", "bay_3_entrance", "bay_4_entrance"],
@@ -43,4 +43,5 @@ path = {
 
     ("start", "start"): ["j1", "bay_3_entrance", "bay_4_entrance", "entrance_lower_b", "entrance_lower_b2", "entrance_lower_b3", "entrance_lower_b4", "entrance_lower_b5", "entrance_lower_b6", "exit_lower_b", "back_right_corner", "ramp", "back_left_corner", "exit_lower_a","entrance_lower_a6", "entrance_lower_a5", "entrance_lower_a4", "entrance_lower_a3", "entrance_lower_a2", "entrance_lower_a", "bay_1_entrance", "bay_2_entrance"]
 }
+
 
